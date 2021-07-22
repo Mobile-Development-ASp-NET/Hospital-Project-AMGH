0b02bd42398d51dd614f3183b137edc4395f0ccc
Estevan Cordero <estevancor@hotmail.com>
2021-07-22 15:44:36 -0400

    Added Migrations

---
+++ Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace Hospital_Project.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Hospital_Project.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Hospital_Project.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
