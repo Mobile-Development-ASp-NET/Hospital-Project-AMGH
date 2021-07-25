07619b5138a101d589b31afe771f8a123c754bad
tingwei xie <tix980@mail.usask.ca>
2021-07-25 12:38:36 -0400

    add surveys and questions tables

---
+++ Models/Questions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Hospital_Project.Models
+{
+    public class Questions
+    {
+        [Key]
+        public int QuestionID { get; set; }
+        public string QuestionTitle { get; set; }
+        public string QuestionyDescription { get; set; }
+
+        public ICollection<Surveys> Surveys { get; set; }
+    }
+}
---
+++ Models/Surveys.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Hospital_Project.Models
+{
+    public class Surveys
+    {
+        [Key]
+        public int SurveyID { get; set; }
+        public string SurveyTitle { get; set; }
+        public string SurveyDescription { get; set; }
+
+        public ICollection<Questions> Questions { get; set; }
+    }
+}
