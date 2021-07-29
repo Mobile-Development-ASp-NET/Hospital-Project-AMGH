21fd91939fd3d911190e1654a3fae7a0d414d96c
albybaby250219 <78487874+albybaby250219@users.noreply.github.com>
2021-07-29 13:35:06 +0530

    added greeting card,admissions updated the dr table to include the fr key in admissions updated the application user

--- Models/DoctorDetails.cs
+++ Models/DoctorDetails.cs
@@ -22,7 +22,8 @@ namespace Hospital_Project.Models
             [ForeignKey("Department")]
             public int DepartmentId { get; set; }
             public virtual Department Department { get; set; }
-        
+            public ICollection<Admission> Admissions { get; set; }
+
     }
     public class DoctorDetailDto
     {
--- Models/IdentityModels.cs
+++ Models/IdentityModels.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.Entity;
+﻿using System.Collections.Generic;
+using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
@@ -9,6 +10,8 @@ namespace Hospital_Project.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        //Users can have many admiited patients
+        public ICollection<Admission> Admissions { get; set; }
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
@@ -37,6 +40,11 @@ namespace Hospital_Project.Models
         //Volunteer Application Entities
         public DbSet<Application> Applications { get; set; }
         public DbSet<Position> Positions { get; set; }
+
+        //greeting card and admission entities
+
+        public DbSet<GreetingCard> GreetingCards { get; set; }
+        public DbSet<Admission> Admissions { get; set; }
 
         public static ApplicationDbContext Create()
         {
