aa6dfc6cf1a3a2d4f2b796266068da114d5fff09
Uditesh <uditesh24jha@gmail.com>
2021-07-31 03:08:12 -0400

    Added blog controller

---
+++ Controllers/BlogDataController.cs
@@ -0,0 +1,136 @@
+﻿using Hospital_Project.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace Hospital_Project.Controllers
+{
+    public class BlogDataController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/BlogData/ListBlogs
+        [HttpGet]
+        [ResponseType(typeof(BlogDto))]
+        public IEnumerable<BlogDto> ListBlogs()
+        {
+            List<Blog> Blogs = db.Blogs.ToList();
+            List<BlogDto> BlogDtos = new List<BlogDto>();
+            Blogs.ForEach(e => BlogDtos.Add(new BlogDto()
+            {
+                BlogID = e.BlogID,
+                Title = e.Title,
+                Content = e.Content
+            }));
+            return BlogDtos;
+        }
+
+
+        // GET: api/BlogData/FindBlog/5
+        [HttpGet]
+        [ResponseType(typeof(BlogDto))]
+        public IHttpActionResult FindBlog(int id)
+        {
+            Blog blog = db.Blogs.Find(id);
+            BlogDto blogDto = new BlogDto()
+            {
+                BlogID = blog.BlogID,
+                Title = blog.Title,
+                Content = blog.Content
+            };
+            if (blog == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(blogDto);
+        }
+
+        // PUT: api/BlogData/UpdateBlog/5
+        [HttpPost]
+        [ResponseType(typeof(void))]
+        public IHttpActionResult UpdateBlog(int id, Blog blog)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != blog.BlogID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(blog).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BlogExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/BlogData/AddBlog
+        [ResponseType(typeof(Blog))]
+        public IHttpActionResult AddBlog(Blog blog)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Blogs.Add(blog);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = blog.BlogID }, blog);
+        }
+
+        // DELETE: api/BlogData/5
+        [ResponseType(typeof(Blog))]
+        public IHttpActionResult DeleteBlog(int id)
+        {
+            Blog blog = db.Blogs.Find(id);
+            if (blog == null)
+            {
+                return NotFound();
+            }
+
+            db.Blogs.Remove(blog);
+            db.SaveChanges();
+
+            return Ok(blog);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool BlogExists(int id)
+        {
+            return db.Blogs.Count(e => e.BlogID == id) > 0;
+        }
+    }
+}
---
+++ Controllers/SubscribedUserDataControllerController.cs
@@ -0,0 +1,150 @@
+﻿using Hospital_Project.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace Hospital_Project.Controllers
+{
+    public class SubscribedUserDataControllerController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/SubscribedUserData/ListSubscribedUsers
+        [HttpGet]
+        [ResponseType(typeof(SubscribedUserDto))]
+        public IEnumerable<SubscribedUserDto> ListSubscribedUsers()
+        {
+            List<SubscribedUser> subscribedUsers = db.SubscribedUsers.ToList();
+            List<SubscribedUserDto> SubscribedUserDtos = new List<SubscribedUserDto>();
+            subscribedUsers.ForEach(e => SubscribedUserDtos.Add(new SubscribedUserDto()
+            {
+                SubscribedUserID = e.SubscribedUserID,
+                Name = e.Name,
+                Email = e.Email
+            }));
+            return SubscribedUserDtos;
+        }
+
+        [HttpGet]
+        [ResponseType(typeof(SubscribedUserDto))]
+        public IHttpActionResult ListSubscribedUsersForBlog(int id)
+        {
+            List<SubscribedUser> SubscribedUsers = db.SubscribedUsers.Where(b => b.Blogs.Any(a => a.BlogID == id)).ToList();
+            List<SubscribedUserDto> SubscribedUserDtos = new List<SubscribedUserDto>();
+
+            SubscribedUsers.ForEach(b => SubscribedUserDtos.Add(new SubscribedUserDto()
+            {
+                SubscribedUserID = b.SubscribedUserID,
+                Name = b.Name,
+                Email = b.Email
+            }));
+            return Ok(SubscribedUserDtos);
+        }
+
+        [HttpGet]
+        [ResponseType(typeof(SubscribedUserDto))]
+        public IHttpActionResult FindSubscribedUser(int id)
+        {
+            SubscribedUser subscribedUser = db.SubscribedUsers.Find(id);
+            SubscribedUserDto subscribedUserDto = new SubscribedUserDto()
+            {
+                SubscribedUserID = subscribedUser.SubscribedUserID,
+                Name = subscribedUser.Name,
+                Email = subscribedUser.Email
+            };
+            if (subscribedUser == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(subscribedUserDto);
+        }
+
+        // PUT: api/SubscribedUserData/UpdateSubscribedUser/5
+        [HttpPost]
+        [ResponseType(typeof(void))]
+        public IHttpActionResult UpdateSubscribedUser(int id, SubscribedUser subscribedUser)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != subscribedUser.SubscribedUserID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(subscribedUser).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SubscribedUserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        [HttpPost]
+        [ResponseType(typeof(SubscribedUser))]
+        public IHttpActionResult AddSubscribedUser(SubscribedUser subscribedUser)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.SubscribedUsers.Add(subscribedUser);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = subscribedUser.SubscribedUserID }, subscribedUser);
+        }
+
+        [HttpPost]
+        [ResponseType(typeof(SubscribedUser))]
+        public IHttpActionResult DeleteSubscribedUser(int id)
+        {
+            SubscribedUser subscribedUser = db.SubscribedUsers.Find(id);
+            if (subscribedUser == null)
+            {
+                return NotFound();
+            }
+
+            db.SubscribedUsers.Remove(subscribedUser);
+            db.SaveChanges();
+
+            return Ok(subscribedUser);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool SubscribedUserExists(int id)
+        {
+            return db.SubscribedUsers.Count(e => e.SubscribedUserID == id) > 0;
+        }
+    }
+}
