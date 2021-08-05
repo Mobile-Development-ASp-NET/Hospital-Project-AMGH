f724e3f0974261d634b33ef007457e492a4572a0
Majdi Nawfal <majdinofal@hotmail.com>
2021-08-05 16:15:36 -0400

    Adding change datatype migration

---
+++ Migrations/202108052014149_change_feedback_datatype.cs
@@ -0,0 +1,16 @@
+namespace Hospital_Project.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class change_feedback_datatype : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
