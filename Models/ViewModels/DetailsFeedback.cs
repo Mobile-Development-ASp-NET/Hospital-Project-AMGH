3d7806a58fa074019a264855dd9b619913936ea9
Majdi Nawfal <majdinofal@hotmail.com>
2021-08-08 04:19:33 -0400

    Adding Feedback Controllers and Views

---
+++ Models/ViewModels/DetailsFeedback.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hospital_Project.Models.ViewModels
+{
+    public class DetailsFeedback
+    {
+        public FeedbackDto SelectedFeedback { get; set; }
+
+        public IEnumerable<DoctorDetailDto> RelatedDoctors { get; set; }
+    }
+}
