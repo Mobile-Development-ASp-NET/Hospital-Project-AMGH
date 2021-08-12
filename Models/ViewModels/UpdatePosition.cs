49f04324c7adc751a5392c971c2b610761f7a212
Estevan Cordero <estevancor@hotmail.com>
2021-08-12 18:46:31 -0400

    Updated position to included DepartmentDto

---
+++ Models/ViewModels/UpdatePosition.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hospital_Project.Models.ViewModels
+{
+    public class UpdatePosition
+    {
+        public PositionDto SelectedPosition { get; set; }
+        public IEnumerable<DepartmentDto> DepartmentOptions { get; set; }
+    }
+}
